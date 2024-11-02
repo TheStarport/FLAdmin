a1e28d669157f2762a93cd2c529b41777bd04881
Nen <sjduttenhefer@gmail.com>
2024-11-02 13:01:15 -0500

    Finish initial refactor of character service.

--- Logic/Services/Database/CharacterService.cs
+++ Logic/Services/Database/CharacterService.cs
@@ -6,14 +6,13 @@ using FlAdmin.Common.Services;
 using LanguageExt;
 using Microsoft.Extensions.Logging;
 using MongoDB.Bson;
-using MongoDB.Driver;
 
 namespace FlAdmin.Logic.Services.Database;
 
 public class CharacterService : ICharacterService
 {
-    private readonly ICharacterDataAccess _characterDataAccess;
     private readonly IAccountDataAccess _accountDataAccess;
+    private readonly ICharacterDataAccess _characterDataAccess;
     private readonly ILogger<CharacterService> _logger;
     private readonly IValidationService _validation;
 
@@ -32,10 +31,7 @@ public class CharacterService : ICharacterService
     {
         var characters = await _characterDataAccess.GetCharactersByFilter(x => x.AccountId == accountId);
 
-        if (characters.Count is 0)
-        {
-            return CharacterError.CharacterNotFound;
-        }
+        if (characters.Count is 0) return CharacterError.CharacterNotFound;
 
         return characters;
     }
@@ -52,10 +48,7 @@ public class CharacterService : ICharacterService
 
     public async Task<Option<CharacterError>> AddCharacter(Character character)
     {
-        if (_validation.ValidateCharacter(character) is false)
-        {
-            return CharacterError.InvalidCharacter;
-        }
+        if (_validation.ValidateCharacter(character) is false) return CharacterError.InvalidCharacter;
 
         return await _characterDataAccess.CreateCharacters(character);
     }
@@ -64,18 +57,12 @@ public class CharacterService : ICharacterService
     {
         var characters = await _characterDataAccess.GetCharactersByFilter(x => x.AccountId == accountId);
 
-        if (characters.Count is 0)
-        {
-            return CharacterError.CharacterNotFound;
-        }
+        if (characters.Count is 0) return CharacterError.CharacterNotFound;
 
         var charNames = characters.Select(character => character.CharacterName).ToList();
 
         var result = await _characterDataAccess.DeleteCharacters(charNames.ToArray());
-        if (result.IsSome)
-        {
-            return result;
-        }
+        if (result.IsSome) return result;
 
 
         List<ObjectId> cleanedCharacterList = new();
@@ -88,14 +75,10 @@ public class CharacterService : ICharacterService
     {
         var charRes = await _characterDataAccess.GetCharacter(character);
         if (charRes.IsLeft)
-        {
-            {
-                return charRes.Match(
-                    Left: err => err,
-                    //Right should never really be reached as we're checking it.
-                    Right: _ => CharacterError.InvalidCharacter);
-            }
-        }
+            return charRes.Match(
+                Left: err => err, Right
+                //Right should never really be reached as we're checking it.
+                : _ => CharacterError.InvalidCharacter);
 
         var ch = charRes.Match<Character>(
             Left: _ => null!,
@@ -104,11 +87,9 @@ public class CharacterService : ICharacterService
 
         var accountRes = await _accountDataAccess.GetAccount(ch.AccountId);
         if (accountRes.IsLeft)
-        {
             return accountRes.Match(
                 Left: err => CharacterError.AccountError,
                 Right: err => CharacterError.AccountError);
-        }
 
         var account = accountRes.Match<Account>(
             Left: _ => null!,
@@ -116,39 +97,29 @@ public class CharacterService : ICharacterService
         account.Characters.Remove(ch.Id);
 
         var res = await _characterDataAccess.DeleteCharacters(ch.CharacterName);
-        if (res.IsSome)
-        {
-            return res;
-        }
+        if (res.IsSome) return res;
 
         var res2 = await _accountDataAccess.UpdateAccount(account.ToBsonDocument());
         return res2.IsSome ? CharacterError.AccountError : new Option<CharacterError>();
     }
 
-
     public async Task<Option<CharacterError>> MoveCharacter(Either<ObjectId, string> character, string newAccountId)
     {
         var charRes = await _characterDataAccess.GetCharacter(character);
         if (charRes.IsLeft)
-        {
-            {
-                return charRes.Match(
-                    Left: err => err,
-                    //Right should never really be reached as we're checking it.
-                    Right: _ => CharacterError.InvalidCharacter);
-            }
-        }
+            return charRes.Match(
+                Left: err => err, Right
+                //Right should never really be reached as we're checking it.
+                : _ => CharacterError.InvalidCharacter);
 
         var ch = charRes.Match<Character>(
             Left: _ => null!,
             Right: ch => ch);
         var oldAccountRes = await _accountDataAccess.GetAccount(ch.AccountId);
         if (oldAccountRes.IsLeft)
-        {
             return oldAccountRes.Match(
                 Left: _ => CharacterError.AccountError,
                 Right: _ => CharacterError.AccountError);
-        }
 
         var oldAccount = oldAccountRes.Match<Account>(
             Left: _ => null!,
@@ -156,11 +127,9 @@ public class CharacterService : ICharacterService
 
         var newAccountRes = await _accountDataAccess.GetAccount(newAccountId);
         if (newAccountRes.IsLeft)
-        {
             return newAccountRes.Match(
                 Left: _ => CharacterError.AccountError,
                 Right: _ => CharacterError.AccountError);
-        }
 
         var newAccount = newAccountRes.Match<Account>(
             Left: _ => null!,
@@ -171,46 +140,41 @@ public class CharacterService : ICharacterService
         newAccount.Characters.Add(ch.Id);
 
         var result1 = await _characterDataAccess.UpdateFieldOnCharacter(character, "accountId", newAccountId);
-        if (result1.IsSome)
-        {
-            return result1;
-        }
+        if (result1.IsSome) return result1;
 
         var result2 = await _accountDataAccess.UpdateFieldOnAccount(oldAccount.Id, "characters", oldAccount.Characters);
-        if (result2.IsSome)
-        {
-            return CharacterError.AccountError;
-        }
+        if (result2.IsSome) return CharacterError.AccountError;
 
         var result3 = await _accountDataAccess.UpdateFieldOnAccount(newAccount.Id, "accounts", newAccount.Characters);
-        if (result3.IsSome)
-        {
-            return CharacterError.AccountError;
-        }
+        if (result3.IsSome) return CharacterError.AccountError;
 
         return new Option<CharacterError>();
     }
 
-    public Task<Option<CharacterError>> UpdateCharacter(Character character)
+    public async Task<Option<CharacterError>> UpdateCharacter(Character character)
     {
-        throw new NotImplementedException();
+        if (_validation.ValidateCharacter(character) is false) return CharacterError.InvalidCharacter;
+
+        return await _characterDataAccess.UpdateCharacter(character.ToBsonDocument());
     }
 
-    public Task<Option<CharacterError>> UpdateFieldOnCharacter<T>(Either<ObjectId, string> character,
+    public async Task<Option<CharacterError>> UpdateFieldOnCharacter<T>(Either<ObjectId, string> character,
         string fieldName,
         T value)
     {
-        throw new NotImplementedException();
+        return await _characterDataAccess.UpdateFieldOnCharacter(character, fieldName, value);
     }
 
-    public Task<Option<CharacterError>> RemoveFieldOnCharacter(Either<ObjectId, string> character, string fieldName)
+    public async Task<Option<CharacterError>> RemoveFieldOnCharacter(Either<ObjectId, string> character,
+        string fieldName)
     {
-        throw new NotImplementedException();
+        return await _characterDataAccess.RemoveFieldOnCharacter(character, fieldName);
     }
 
-    public Task<Option<CharacterError>> AddFieldOnCharacter<T>(Either<ObjectId, string> character, string fieldName,
+    public async Task<Option<CharacterError>> AddFieldOnCharacter<T>(Either<ObjectId, string> character,
+        string fieldName,
         T value)
     {
-        throw new NotImplementedException();
+        return await _characterDataAccess.CreateFieldOnCharacter(character, fieldName, value);
     }
 }
