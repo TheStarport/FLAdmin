570e4de34a96c4736d9f9e8c1dcc61f0be81d414
Nen <sjduttenhefer@gmail.com>
2024-11-03 13:51:56 -0600

    added error parse extension to character error
    also added the new account errors to the account error parsing.

--- Service/Extensions/ResultExtensions.cs
+++ Service/Extensions/ResultExtensions.cs
@@ -28,7 +28,31 @@ public static class ResultExtensions
             AccountError.ElementTypeMismatch => controller.BadRequest(accountError.GetEnumDescription()),
             AccountError.AccountAlreadyHasUsername => controller.Conflict(accountError.GetEnumDescription()),
             AccountError.FieldDoesNotExist => controller.NotFound(accountError.GetEnumDescription()),
+            AccountError.AccountIdAlreadyExists => controller.Conflict(accountError.GetEnumDescription()),
+            AccountError.AccountIsProtected => controller.Unauthorized(accountError.GetEnumDescription()),
+            AccountError.FieldIsProtected => controller.Unauthorized(accountError.GetEnumDescription()),
+            AccountError.FieldAlreadyExists => controller.Conflict(accountError.GetEnumDescription()),
             _ => throw new ArgumentOutOfRangeException(nameof(accountError), accountError, null)
         };
     }
+
+    public static IActionResult ParseAccountError(this CharacterError characterError, ControllerBase controller)
+    {
+        return characterError switch
+        {
+            CharacterError.DatabaseError =>  new ObjectResult(StatusCodes.Status500InternalServerError),
+            CharacterError.CharacterAlreadyExists => controller.Conflict(characterError.GetEnumDescription()),
+            CharacterError.CharacterIdIsNull => controller.BadRequest(characterError.GetEnumDescription()),
+            CharacterError.CharacterNotFound => controller.NotFound(characterError.GetEnumDescription()),
+            CharacterError.FieldAlreadyExists => controller.Conflict(characterError.GetEnumDescription()),
+            CharacterError.FieldIsProtected => controller.Unauthorized(characterError.GetEnumDescription()),
+            CharacterError.FieldDoesNotExist => controller.NotFound(characterError.GetEnumDescription()),
+            CharacterError.ElementTypeMismatch => controller.BadRequest(characterError.GetEnumDescription()),
+            CharacterError.InvalidCharacter => controller.BadRequest(characterError.GetEnumDescription()),
+            CharacterError.AccountError => controller.BadRequest(characterError.GetEnumDescription()),
+            CharacterError.CharacterIsLoggedIn => controller.BadRequest(characterError.GetEnumDescription()),
+            _ => throw new ArgumentOutOfRangeException(nameof(characterError), characterError, null)
+        };
+    }
+    
 }
